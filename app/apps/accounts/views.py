ce1adda54209eb0ee12fd78ac813ccf080c16785
PatakIN <git@patakin.space>
2024-03-01 21:37:55 +0500

    feat: генерация страниц с ошибкой

--- app/apps/accounts/views.py
+++ app/apps/accounts/views.py
@@ -22,6 +22,7 @@ from django.contrib.sites.models import Site
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.contrib.auth import login
+from django.shortcuts import render
 
 from apps.accounts.forms.password import (
     AccountsForgotPasswordForm,
@@ -36,6 +37,51 @@ from apps.accounts.models import Accounts
 
 
 # Create your views here.
+
+
+def tr_handler404(request, exception):
+    """
+    Обработка ошибки 404
+    """
+    return render(
+        request=request,
+        template_name="error_page.html",
+        status=404,
+        context={
+            "title": "Страница не найдена: 404",
+            "error_message": "К сожалению такая страница была не найдена, или перемещена",
+        },
+    )
+
+
+def tr_handler500(request):
+    """
+    Обработка ошибки 500
+    """
+    return render(
+        request=request,
+        template_name="error_page.html",
+        status=500,
+        context={
+            "title": "Ошибка сервера: 500",
+            "error_message": "Внутренняя ошибка сайта, вернитесь на главную страницу, отчет об ошибке мы направим администрации сайта",
+        },
+    )
+
+
+def tr_handler403(request, exception):
+    """
+    Обработка ошибки 403
+    """
+    return render(
+        request=request,
+        template_name="error_page.html",
+        status=403,
+        context={
+            "title": "Ошибка доступа: 403",
+            "error_message": "Доступ к этой странице ограничен",
+        },
+    )
 
 
 class IndexView(TemplateView):
--- app/core/urls.py
+++ app/core/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+handler403 = "apps.accounts.views.tr_handler403"
+handler404 = "apps.accounts.views.tr_handler404"
+handler500 = "apps.accounts.views.tr_handler500"
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.accounts.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("apps.accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
