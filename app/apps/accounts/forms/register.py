118612e9d2b48e9bc9c77435b3679b0f53b63d86
PatakIN <git@patakin.space>
2024-03-02 17:15:42 +0500

    fix: мелкие исправления вывода

--- app/apps/accounts/forms/register.py
+++ app/apps/accounts/forms/register.py
@@ -7,22 +7,43 @@ class AccountsRegisterForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = Accounts
-        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
+        fields = UserCreationForm.Meta.fields + ("email", "first_name", "last_name")
 
     def clean_email(self):
-        email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and Accounts.objects.filter(email=email).exclude(username=username).exists():
-            raise forms.ValidationError('Email is already in use')
+        email = self.cleaned_data.get("email")
+        username = self.cleaned_data.get("username")
+        if (
+            email
+            and Accounts.objects.filter(email=email).exclude(username=username).exists()
+        ):
+            raise forms.ValidationError("Email используется другим пользователем")
         return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if len(username) < 5:
+            raise forms.ValidationError("Логин должен быть не менее 5 символов")
+        return username
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields['username'].widget.attrs.update({"placeholder": 'Придумайте свой логин'})
-            self.fields['email'].widget.attrs.update({"placeholder": 'Введите свой email'})
-            self.fields['first_name'].widget.attrs.update({"placeholder": 'Ваше имя'})
-            self.fields["last_name"].widget.attrs.update({"placeholder": 'Ваша фамилия'})
-            self.fields['password1'].widget.attrs.update({"placeholder": 'Придумайте свой пароль'})
-            self.fields['password2'].widget.attrs.update({"placeholder": 'Повторите придуманный пароль'})
-            self.fields[field].widget.attrs.update({"class": "form-control", "autocomplete": "off"})
+            self.fields["username"].widget.attrs.update(
+                {"placeholder": "Придумайте свой логин"}
+            )
+            self.fields["email"].widget.attrs.update(
+                {"placeholder": "Введите свой email"}
+            )
+            self.fields["first_name"].widget.attrs.update({"placeholder": "Ваше имя"})
+            self.fields["last_name"].widget.attrs.update(
+                {"placeholder": "Ваша фамилия"}
+            )
+            self.fields["password1"].widget.attrs.update(
+                {"placeholder": "Придумайте свой пароль"}
+            )
+            self.fields["password2"].widget.attrs.update(
+                {"placeholder": "Повторите придуманный пароль"}
+            )
+            self.fields[field].widget.attrs.update(
+                {"class": "form-control", "autocomplete": "off"}
+            )
--- app/apps/accounts/forms/update.py
+++ app/apps/accounts/forms/update.py
@@ -7,18 +7,33 @@ from apps.accounts.models import Accounts
 class AccountsUpdateForm(forms.ModelForm):
     class Meta:
         model = Accounts
-        fields = ['username', 'email', 'first_name', 'last_name', 'bio', 'birth_date', 'avatar']
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "bio",
+            "birth_date",
+            "avatar",
+            "phone",
+            "address",
+        ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control', 'autocomplete': 'off'})
+            self.fields[field].widget.attrs.update(
+                {"class": "form-control", "autocomplete": "off"}
+            )
 
     def clean_email(self):
-        email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and Accounts.objects.filter(email=email).exclude(username=username).exists():
-            raise forms.ValidationError('Email is already in use')
+        email = self.cleaned_data.get("email")
+        username = self.cleaned_data.get("username")
+        if (
+            email
+            and Accounts.objects.filter(email=email).exclude(username=username).exists()
+        ):
+            raise forms.ValidationError("Email используется другим пользователем")
         return email
 
 
@@ -26,4 +41,6 @@ class AccountsPasswordForm(SetPasswordForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control', 'autocomplete': 'off'})
+            self.fields[field].widget.attrs.update(
+                {"class": "form-control", "autocomplete": "off"}
+            )
